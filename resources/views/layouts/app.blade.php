abf6191d395d7a7d3d9032faf247855eddc1062b
saifdev1642 <saifdev1642@gmail.com>
2018-05-20 16:37:43 +0600

    Form Components & views Updated

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Picxalbum</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/css/foundation.css">
+</head>
+<body>
+    <div class="row">
+        @yield('content')
+    </div>
+</body>
+</html>
